d2f631bbe8311d419f0128b2aef9493fc3bfa5ab
scar <68137719+skies-s@users.noreply.github.com>
2021-07-17 20:51:09 +0600

    Create node.js

---
+++ node.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get('/', (request, response) => {
+  console.log(Date.now() + ' Ping Received');
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+const { Client } = require('discord.js');
+const client = new Client();
+
+client.on('ready', () => {
+  client.user
+    .setPresence({ activity: { name: 'SCAR IS GOD' }, status: 'online' })
+    .catch(console.error);
+  console.log(`Logged in as ${client.user.username}`);
+});
+
+client.login(process.env.TOKEN);
